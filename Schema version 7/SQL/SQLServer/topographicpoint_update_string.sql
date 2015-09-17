27d5a933671f17368fe13f21d2655a0d53d4f1b6
Tim Martin <tim.martin@ordnancesurvey.co.uk>
2015-09-17 09:50:46 +0100

    Tidied up the Positioned Noncon synatx

--- Schema version 7/SQL/SQLServer/topographicpoint_update_string.sql
+++ Schema version 7/SQL/SQLServer/topographicpoint_update_string.sql
@@ -10,7 +10,7 @@ CASE
 	WHEN featurecode = 10158 THEN 'Rail Point'
 	WHEN featurecode = 10050 THEN 'Positioned Coniferous Tree Point'
 	WHEN featurecode = 10094 THEN 'Landform Point'
-	WHEN featurecode = 10080 AND descriptiveterm = '{Structure}' THEN 'Historic Point'
+	WHEN featurecode = 10080 AND descriptiveterm = 'Structure' THEN 'Historic Point'
 	WHEN featurecode = 10129 THEN 'Boundary Post Point'
 	WHEN featurecode = 10186 AND  descriptiveterm = 'Triangulation Point Or Pillar' THEN 'Triangulation Point Or Pillar Point'
 	WHEN featurecode = 10191 THEN 'Structure Point'
@@ -19,7 +19,6 @@ CASE
 	WHEN featurecode = 10209 THEN 'Tidal Water Point'
 	WHEN featurecode = 10100 THEN 'Disused Feature Point'
 	WHEN featurecode = 10159 AND descriptiveterm = 'Switch' THEN 'Rail Switch Point'
-	WHEN featurecode = 10080 AND descriptiveterm = 'Positioned Nonconiferous Tree' THEN 'Positioned Nonconiferous Tree Point'
 	WHEN (featurecode = 10120 OR featurecode = 10176 OR featurecode = 10159) THEN 'Inland Water Point'
 	ELSE 'Unclassified'
 END,
@@ -35,7 +34,7 @@ CASE
 	WHEN featurecode = 10158 THEN 8
 	WHEN featurecode = 10050 THEN 9
 	WHEN featurecode = 10094 THEN 10
-	WHEN featurecode = 10080 AND descriptiveterm = '{Structure}' THEN 11
+	WHEN featurecode = 10080 AND descriptiveterm = 'Structure' THEN 11
 	WHEN featurecode = 10129 THEN 12
 	WHEN featurecode = 10186 AND  descriptiveterm = 'Triangulation Point Or Pillar' THEN 13
 	WHEN featurecode = 10191 THEN 5
@@ -44,7 +43,6 @@ CASE
 	WHEN featurecode = 10209 THEN 16
 	WHEN featurecode = 10100 THEN 17
 	WHEN featurecode = 10159 AND descriptiveterm = 'Switch' THEN 18
-	WHEN featurecode = 10080 AND descriptiveterm = 'Positioned Nonconiferous Tree' THEN 3
 	WHEN (featurecode = 10120 OR featurecode = 10176 OR featurecode = 10159) THEN 4
 	ELSE 99
 END;
